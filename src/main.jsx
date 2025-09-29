cf3de39cfb09e9211f281f560071b149ce40f429
ShubhpreetKDhariwal <shubhpreetkdhariwal@gmail.com>
2025-09-29 13:28:52 +0530

    Organized files

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext'
-import { NotificationProvider } from './contexts/NotificationContext'
+import { AuthProvider } from './contexts/AuthContext.jsx'
+import { NotificationProvider } from './contexts/NotificationContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
